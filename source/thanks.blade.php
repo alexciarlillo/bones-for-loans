0826034a27c3a6861563ffaf462e752fdac65864
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2019-01-23 07:56:42 -0500

    add thanks page

---
+++ source/thanks.blade.php
@@ -0,0 +1,15 @@
+@extends('_layouts.master')
+
+@section('content')
+    <div class="container mx-auto mb-12">
+        <h1 class="font-headline text-center mt-12 mb-8 text-neutral-darkest">Bones For Loans</h1>
+
+        <div class="max-w-sm md:max-w-md rounded overflow-hidden shadow-lg bg-neutral-lighter mt-6 mx-auto border-neutral-light border-2">
+            <div class="px-6 py-4">
+                <div class="text-xl mb-2">Thanks! You're message has been submitted and I will try to get back to you as soon as I can.</div>
+                <div class="text-lg italic">- Kaili</div>
+            </div>
+        </div>
+
+    </div>
+@endsection
